7b1faaeab11b003f90975ed8a88d3b2c82008528
Kostiantyn Shrubenko <kshrubenko.kse.org.ua>
2024-11-17 21:34:12 +0200

    Overall structure of the program

--- olympics.py
+++ olympics.py
@@ -1,2 +1,34 @@
-if __name__ == '__main__':
+arguments = {
+    'input_filename': '',
+    'output_filename': '',
+    'country': '',
+    'year': ''
+}
+
+def parse_command_line():
+    #get command line arguments
     pass
+
+def read_data_from_file(filename: str):
+    #read data from file and return as a dictionnary
+    pass
+
+def is_country_and_year_valid(country: str, year: str):
+    #check whether arguments country and year exists in a data sample 
+    pass
+
+def get_medals_by_country_year(country: str, year: str):
+    pass
+
+def print_medals(file_name: str = None):
+    pass
+
+if __name__ == '__main__':
+    parse_command_line()
+    data = read_data_from_file(arguments['input_filename'])
+    if not is_country_and_year_valid(arguments['country'], arguments['year']):
+        print("Not valid country or there was no olympiad in given year")
+        exit()
+    medals = get_medals_by_country_year(arguments['country'], arguments['year'])
+    print_medals(medals)
+
