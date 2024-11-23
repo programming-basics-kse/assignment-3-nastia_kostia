528aa7e6d7463af36a37792509668e5af782b3f5
Kostiantyn Shrubenko <kshrubenko.kse.org.ua>
2024-11-23 23:56:09 +0200

    Added interactive mode

--- olympics.py
+++ olympics.py
@@ -10,8 +10,9 @@ def parse_command_line():
     )
 
     parser.add_argument('filename', help='Path to the data file')
-    parser.add_argument('-m', '--medals', nargs=2, required=True, help='country abbreviation and year')
+    parser.add_argument('-m', '--medals', nargs=2, help='country abbreviation and year')
     parser.add_argument('-o', '--output', help='The name of the output file')
+    parser.add_argument('-i', '--interactive', action='store_const', const=True, help='Enters interactive mode to access data for given country')
     return parser.parse_args()
 
 
@@ -24,12 +25,24 @@ def read_data_from_file(filename: str):
 if __name__ == '__main__':
     args = parse_command_line()
     data = read_data_from_file(args.filename)
+    is_interactive = False
     if args.medals:
         parser = parser.Top10Medalists(data, args.medals)
+    elif args.interactive:
+        parser = parser.Interactive(data)
+        is_interactive = True
     elif args.total:
         parser = parser.Total(data, args.medals)
     elif args.overall:
         parser = parser.Overall(data, args.medals)
+
+    if is_interactive:
+        while(1):
+            country = input("Enter country name or code: ")
+            if country == 'q':
+                exit()
+            parser.parse_data(country)
+            parser.print_data()
 
     if not parser.check_data():
         exit()
--- parser.py
+++ parser.py
@@ -77,3 +77,68 @@ class Overall(Parser):
     
     def parse_data(self):
         pass
+
+
+class Interactive(Parser):
+    def __init__(self, data):
+        self.data = data
+
+    def check_data(self):
+        pass
+    
+    def parse_data(self, country):
+        class Olympiad():
+            def __init__(self, city=''):
+                self.city = city
+                self.total_medals = 0
+                self.medals = {
+                    'Gold': 0,
+                    'Silver': 0,
+                    'Bronze': 0
+                }
+            def add_medal(self, medal):
+                if medal != 'NA':
+                    self.medals[medal] += 1
+                    # print(self.medals)
+                self.total_medals += 1
+            def get_medals_count(self):
+                return self.medals
+            def get_total_medals(self):
+                return self.total_medals
+            def get_city(self):
+                return self.city
+            def __lt__(self, other_olympiad):
+                return self.total_medals < other_olympiad.total_medals
+
+        years = {}
+        self.medals = []
+        for row in self.data:
+            if not row['Team'] == country and not row['NOC'] == country:
+                continue
+            if not int(row['Year']) in years:
+                years[int(row['Year'])] = Olympiad(row['City'])
+            years[int(row['Year'])].add_medal(row['Medal'])
+
+        worst_olympiad = min(years, key=years.get)
+        best_olympiad = max(years, key=years.get)
+        first_olympiad = min(list(years.keys()))
+
+        avg_medals = Olympiad().get_medals_count()
+        for olympiad in years.values():
+            medals = olympiad.get_medals_count()
+            for medal, count in medals.items():
+                avg_medals[medal] += count
+        for medal, count in avg_medals.items():
+            avg_medals[medal] //= len(years)
+        
+        self.table_header = "{:20s} | \t {:5s} |\t {:15s} |\t {}\n".format("Type", "Year", "City", "Medals")
+        self.table = "{:20s} | \t {:5s} |\t {:15s} |\t {}\n".format(
+            "First Olympiad", str(first_olympiad), years[first_olympiad].get_city(), years[first_olympiad].get_total_medals())
+        self.table += "{:20s} | \t {:5s} |\t {:15s} |\t {}\n".format(
+            "Best Olympiad", str(best_olympiad), years[best_olympiad].get_city(), years[best_olympiad].get_total_medals())
+        self.table += "{:20s} | \t {:5s} |\t {:15s} |\t {}\n".format(
+            "Worst Olympiad", str(worst_olympiad), years[worst_olympiad].get_city(), years[worst_olympiad].get_total_medals())
+        self.table += "\n{}\n{:25s} {:10s} |\t {:10s} |\t {:10s}\n".format(
+            "Avarage Number of Medals:", '', "Gold", "Silver", "Bronze") 
+        self.table += "{:25s} {:10s} |\t {:10s} |\t {:10s}".format(
+            '', str(avg_medals["Gold"]), str(avg_medals["Silver"]), str(avg_medals["Bronze"])) 
